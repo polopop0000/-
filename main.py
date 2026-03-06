ab1b59f462c387310936722f7c3512344d45e6fa
polopop0000 <polina@manzhosova.ru>
2026-03-06 22:18:46 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import asyncio
+import sys
+from aiogram import Bot, Dispatcher
+
+from app.handlers import router
+
+if sys.platform.startswith("win"): #проверка запуска кода на windows
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy()) #исправление проблемы асинхронности на windows
+
+async def main(): #функция запуска бота
+    bot = Bot(token='8384710369:AAEYlgAbE2_oD7d9_1j17QpMoFb23cjmnHc') #создание объекта бота и импорт токена
+    dp = Dispatcher()
+    dp.include_router(router) #подключение всех обработчиков из hendlers.py
+    await dp.start_polling(bot) #запуск бота
+
+
+if __name__ == '__main__': #проверка запуска файла
+    try:
+        asyncio.run(main()) #запуск асинхронной функции main()
+    except KeyboardInterrupt: #остановка бота(Ctrl+C)
+        print('Бот выключен')
